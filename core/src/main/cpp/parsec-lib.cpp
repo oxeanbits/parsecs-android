62588ff18f55b55820ca91bb5e4794c7506677b9
Nilton Vasques <nilton.vasques@gmail.com>
2021-08-09 20:22:47 -0300

    Fix json malformed for link(str, str) equations
    Replace quotes before build the json by escaped quotes.

--- core/src/main/cpp/parsec-lib.cpp
+++ core/src/main/cpp/parsec-lib.cpp
@@ -37,6 +37,26 @@ Value Calc(string input) {
     return ans;
 }
 
+void replaceAll(std::string& source, const std::string& from, const std::string& to) {
+    std::string newString;
+    newString.reserve(source.length());  // avoids a few memory allocations
+
+    std::string::size_type lastPos = 0;
+    std::string::size_type findPos;
+
+    while(std::string::npos != (findPos = source.find(from, lastPos)))
+    {
+        newString.append(source, lastPos, findPos - lastPos);
+        newString += to;
+        lastPos = findPos + from.length();
+    }
+
+    // Care for the rest after last occurrence
+    newString += source.substr(lastPos);
+
+    source.swap(newString);
+}
+
 jstring CalcJson(JNIEnv *env, string input) {
     ParserX parser(pckALL_NON_COMPLEX);
 
@@ -52,7 +72,11 @@ jstring CalcJson(JNIEnv *env, string input) {
         parser.SetExpr(input);
         ans = parser.Eval();
 
-        ss << _T("\"val\": \"") << ans.AsString() << _T("\"");
+        std::string ansString = ans.AsString();
+
+        replaceAll(ansString, "\"", "\\\"");
+
+        ss << _T("\"val\": \"") << ansString << _T("\"");
         ss << _T(",\"type\": \"") << ans.GetType() << _T("\"");
     }
     catch(ParserError &e)
