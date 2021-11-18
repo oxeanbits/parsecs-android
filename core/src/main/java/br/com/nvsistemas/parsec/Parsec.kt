6df5dc958fff0d709e7beccf4334b7a6d406a24b
Rodrigo Ferreira <rodrigo_fpinho@hotmail.com>
2021-11-17 21:33:14 -0300

    new eval

--- core/src/main/java/br/com/nvsistemas/parsec/Parsec.kt
+++ core/src/main/java/br/com/nvsistemas/parsec/Parsec.kt
@@ -35,6 +35,29 @@ class Parsec {
         return Value("", "")
     }
 
+    fun evalValues(equations: Map<Int, String>): Map<Int, Value> {
+        val values = mutableMapOf<Int, Value>()
+
+        equations.forEach {
+            val result = nativeEvalJson(it.value)
+
+            if(result.startsWith("{\"error\"")) {
+                val error = result.substring(11, result.length - 1)
+                throw UnsupportedOperationException(error)
+            }
+
+            try {
+                val json = JSONObject(result)
+                values[it.key] = Value(json.optString("val", ""),
+                    json.optString("type", ""))
+            } catch (ex: JSONException) {
+                Log.e(Parsec::class.java.simpleName, "Error on try parser json.", ex)
+            }
+        }
+
+        return values
+    }
+
     private external fun nativeEval(formula: String): String
 
     private external fun nativeEvalJson(formula: String): String
